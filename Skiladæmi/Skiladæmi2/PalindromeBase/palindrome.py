87ef225982ce9e64efa0d1727ac73250d403f2f3
Helena <helenaj18@ru.is>
2020-02-14 12:58:26 +0000

    tilraun palindrome

--- Skiladæmi/Skiladæmi2/PalindromeBase/palindrome.py
+++ Skiladæmi/Skiladæmi2/PalindromeBase/palindrome.py
@@ -11,24 +11,58 @@ def print_to_screen(head):
         print("")
 
 def palindrome(head):
+    
+    head, node = splitList(head, head)
+    node = reverseList(node)
+    
+    while head != None and node != None:
+        if head.data != node.data:
+            return False
+        head = head.next
+        node = node.next
 
-    str_front = getString(head)
-    reverse_node = reverseList(head)
-    str_back = getString(reverse_node)
+    return True
 
-    if str_front == str_back:
-        return True
 
-    return False
+# def splitList(head):
+#     '''Splits a list in the middle'''
+#     node = head
+#     node_half = head
+
+
+#     while node.next != None and node.next.next != None:
+#         node = node.next.next
+#         node_half = node_half.next
+#     else:
+#         node = node_half.next
+#         node_half.next = None
+
+#         return head,node
+
+def splitList(head, node_half):
+    '''Splits a list in the middle'''
+    
+    node = head
+
+    if node.next == None or node.next.next == None:
+        node = node_half.next
+        node_half.next = None
+        return head, node
+
+    else:
+        splitList(node.next.next, node_half.next)
 
 
 def reverseList(head):
     if head.next == None:
         return head
+    
 
     node = reverseList(head.next)
+
     head.next.next = head
     head.next = None
+
     return node
 
 def getString(head):
